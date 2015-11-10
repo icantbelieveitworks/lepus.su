5303e913edff41a6bd4682631f476b3938b443b1
poiuty <poiuty@lepus.su>
2015-11-10 09:16:31 +0300

    add link

--- kvm/php/control.php
+++ kvm/php/control.php
@@ -1,5 +1,6 @@
 <?php
-// https://www.linux.org.ru/forum/admin/12097297
+// https://www.linux.org.ru/forum/admin/12097297 (start non-root)
+// https://poiuty.com/index.php?title=%D0%9A%D0%BE%D0%BC%D0%BF%D0%B8%D0%BB%D0%B8%D1%80%D1%83%D0%B5%D0%BC_libvirt-php (build libvirt-php)
 $uri = "qemu+unix:///system"; $credentials = [VIR_CRED_AUTHNAME => "", VIR_CRED_PASSPHRASE => ""]; $res = libvirt_connect($uri,false,$credentials);
 
 function get_status($name){

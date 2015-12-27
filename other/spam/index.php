9c8abfda22564b176f19a64ac2c04c97ebb678c1
poiuty <poiuty@lepus.su>
2015-12-27 04:30:25 +0300

    update spam index

--- other/spam/index.php
+++ other/spam/index.php
@@ -6,6 +6,11 @@ function clamav_check($file){
 		$i = explode("\n", $data);
 		$j = explode(":", $i[0]);
 		$a = ['send' => 'no', 'info' => trim($j[1])];		
+	}else{
+		$data = file_get_contents($file);
+		if(count(file($file)) < 10 && strlen($data) > 10000){ // обычно такой скрипт это 1-10 строчек и много много символов.
+			$a = ['send' => 'no', 'info' => 'possible spam bot'];	
+		}
 	}
 	unlink($file);
 	return json_encode($a);
@@ -14,11 +19,12 @@ function clamav_check($file){
 function clamav_start(){
 	$base = '/var/www/tmp';
 	$hash = hash('sha256', file_get_contents($_FILES["file"]["tmp_name"]));
-	$name_dir = substr($hash, 0, 2);
-	if(!is_dir("$base/$name_dir")){
-		mkdir("$base/$name_dir");
-	}
-	move_uploaded_file($_FILES["file"]["tmp_name"], "$base/$name_dir/$hash");
-	return clamav_check("$base/$name_dir/$hash");
+	//$name_dir = substr($hash, 0, 2);
+	//if(!is_dir("$base/$name_dir")){
+	//	mkdir("$base/$name_dir");
+	//}
+	move_uploaded_file($_FILES["file"]["tmp_name"], "$base/$hash");
+	return clamav_check("$base/$hash");
 }
-echo clamav_start();
+
+if(!empty($_POST) && !empty($_FILES)) echo clamav_start();

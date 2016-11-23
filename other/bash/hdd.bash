a1fcfd779dc344461435f199e30f38aa8b49c72c
lepus <poiuty@lepus.su>
2016-11-23 03:45:04 +0300

    update hdd

--- other/bash/hdd.bash
+++ other/bash/hdd.bash
@@ -1,8 +1,12 @@
 #!/bin/bash
-# cron: 01 12 * * * root cd /root/lepus && ./hdd.bash
 # apt-get install smartmontools hdparm curl -y
+# SHELL=/bin/sh
+# PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
+# MAILTO=""
+# HOME=/
+# 01 12 * * * root cd /root/lepus && ./hdd.bash
 
-function lepusHelpMe {
+lepusHelpMe() {
 	curl -4 https://lepus.su/public/api/hdd.php
 	exit 1
 }
@@ -13,6 +17,8 @@ LOG2=$(cat /var/log/syslog | grep "EH complete")
 
 if [[ !  -z  "$LOG1"  ]] || [[ !  -z  "$LOG2"  ]] || [[ !  -z  "$MDSTAT"  ]] ; then
 	lepusHelpMe
+	echo "log or mdstat"
+	exit 1
 fi
 
 re='^[a-z/]+$'
@@ -20,11 +26,13 @@ ls /dev/sd*| while read DISK; do
 	if [[ $DISK =~ $re ]] ; then
 		SMARTCTL=$(smartctl --all $DISK | grep "No Errors Logged")
 		if [[  -z  "$SMARTCTL"  ]] ; then
+			echo "SMART $DISK"
 			lepusHelpMe
 		fi
 		
 		SMARTCTL=$(smartctl --all $DISK | grep Reallocated_Sector_Ct | awk '{print $10}')
 		if [[ "$SMARTCTL" > "50" ]] ; then
+			echo "Reallocated $DISK "
 			lepusHelpMe
 		fi
 	fi

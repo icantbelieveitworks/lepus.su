a4ce01bd402b6c7a06b89beec266d91cd843abb8
lepus <poiuty@lepus.su>
2017-01-20 20:25:21 +0300

    cp fix dns

--- cp/build.sh
+++ cp/build.sh
@@ -1,4 +1,5 @@
 # https://blog.filippo.io/shrink-your-go-binaries-with-this-one-weird-trick/
 # https://github.com/pwaller/goupx
+export GOPATH=~/lepuscp
 go build -ldflags="-s -w" lepus.go
 ./goupx lepus

7c7ce599ec44ef8d4f75bc0be91c4703cdcd7939
poiuty <poiuty@lepus.su>
2018-04-12 10:55:58 +0300

    avg 90d

--- site/private/cron/currency.php
+++ site/private/cron/currency.php
@@ -5,15 +5,19 @@ require_once($_SERVER['DOCUMENT_ROOT'].'/private/init/memcache.php');
 require_once($_SERVER['DOCUMENT_ROOT'].'/private/init/session.php');
 require_once($_SERVER['DOCUMENT_ROOT'].'/private/func.main.php');
 
-$i = file_get_contents('https://www.ecb.europa.eu/stats/eurofxref/eurofxref-daily.xml');
+$i = file_get_contents('https://www.ecb.europa.eu/stats/eurofxref/eurofxref-hist-90d.xml');
 $xml = simplexml_load_string($i);
-foreach($xml->Cube->Cube->Cube as $rate){
-	if($rate['currency'] == 'RUB'){
-		$j = floatval($rate['rate']);
-		if(is_numeric($j)){
-			$update = $db->prepare("UPDATE `currency` SET `val` =:rate WHERE `name` = 'EUR'");
-			$update->bindParam(':rate', $j, PDO::PARAM_STR);
-			$update->execute();
+$count = count($xml->Cube->Cube);
+for ($i=0;$i<$count;$i++) {
+	foreach($xml->Cube->Cube[$i]->Cube as $rate) {
+		if($rate['currency'] == 'RUB'){
+			$j += floatval($rate['rate']);
 		}
 	}
+}
+$j = round($j/$count, 2);
+if(is_numeric($j) && $j > 40 && $j < 160){
+	$update = $db->prepare("UPDATE `currency` SET `val` =:rate WHERE `name` = 'EUR'");
+	$update->bindParam(':rate', $j, PDO::PARAM_STR);
+	$update->execute();
 }
